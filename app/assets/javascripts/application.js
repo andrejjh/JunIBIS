c1b1ec359189e1f1632d5585ae1277b32ff08057
Andre Heughebaert <andrejjh@gmail.com>
2015-08-03 15:12:17 +0200

    Adding pre-compiled assets.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap-sprockets
+//= require bootstrap
 //= require ZoomifyImageViewerExpress-min
 //= require_tree .
