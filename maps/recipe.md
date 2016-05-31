5959c36adaa329091324b92adb744ce04b64c7da
Andre Heughebaert <andrejjh@gmail.com>
2016-05-31 11:36:25 +0200

    adding recipe.md, moves.sql.

---
+++ maps/recipe.md
@@ -0,0 +1,17 @@
+# updating moves
+## Spreadsheet
+1. Open input/Eric/1815Moves28.ods
+2. Save-a-copy of positions sheet into CoBPos.csv -sep=;
+3. Save-a-copy of locations sheet into CoBLoc.csv -sep=;
+
+## Sqlite
+execute moves.sql and run two ruby programs
+
+## Install dataset on cartoDB
+* add new dataset
+* add a basic data visualization map
+
+## Changes to JunIBIS application
+* change cobMoves<prev> to cobMoves<new> in dynamic, static and show
+* change version in application layout
+* git commit and heroku push
--- maps/sql/moves.sql
+++ maps/sql/moves.sql
@@ -8,22 +8,24 @@
 
 # Import data
 .import csv/1815/1815units.csv units
-.import csv/MovesLoc.csv locations
-.import csv/MovesPos.csv positions
+.import csv/CoBLoc.csv locations
+.import csv/MSJLoc.csv locations
+.import csv/CoBPos.csv positions
+.import csv/MSJPos.csv positions
 .import csv/Battles.csv battles
 .import csv/BattleUnits.csv battleUnits
 .import csv/Ranks.csv ranks
+.import csv/icons.csv icons
 #.import csv/1815/1815people.csv people
 #.import csv/1815/1815peopleBLC.csv people
 #.import csv/1815/1815peopleFAN.csv people
 #.import csv/1815/1815peoplePLR.csv people
 
-.import csv/icons.csv icons
 
 update units set parentid=null where parentid="";
 update positions set departure=null where departure="";
-update positions set arrival=null where arrival="";	
-	
+update positions set arrival=null where arrival="";
+
 # Create indexes
 create unique index units_id on units (id);
 create unique index locations_id on locations (id);
@@ -32,19 +34,24 @@ create unique index battles_id on battles (id);
 create unique index ranks_id on ranks (id);
 create unique index icons_id on icons (id);
 #create unique index people_hash on people(hash);
-			
+
 
 # Quality control
 select * from units where parentid not in (select id from units);
 select * from units where rank not in (select id from ranks);
 select * from positions where unitid not in (select id from units);
 select * from positions where locid not in (select id from locations);
+select * from positions where arrival not like '1815/06/%' or departure not like '1815/06/%';
+select * from positions where substr(arrival,9,2) not in ('14', '15','16','17','18','19','20','21', '22');
+select * from positions where substr(departure,9,2) not in ('14', '15','16','17','18','19','20','21', '22');
+select * from positions where arrival is null and departure is null;
+
 select * from battleUnits where unitid not in (select id from units);
 select * from battleUnits where battleid not in (select id from battles);
 #select * from people where unit_id not in (select id from units);
 
 # allUnits for 15CoB battle
-insert into battleUnits select distinct 'CoB', unitID from positions order by unitID; 
+insert into battleUnits select distinct 'CoB', unitID from positions order by unitID;
 
 
 #run ruby people.rb
@@ -53,21 +60,18 @@ insert into battleUnits select distinct 'CoB', unitID from positions order by un
 #.once csv/1815/1815units.csv
 #select * from units;
 
-
-	# run ruby moves.rb CoB
+create table moves (unitID varchar(16), part int, step int, datetime text, latitude real , longitude real, army int, flat real, flong real);
+# run ruby moves.rb CoB
 update moves set flat=round(latitude, 4), flong=round(longitude,4);
-update moves set army = case(substr(unitid,1,1)) when 'G' then 1 when 'F' then 1 when 'P' then 2 when 'B' then 3 when 'H' then 4 when 'R' then 5 else 6 end;	
-	# run ruby stacking.rb
+update moves set army = case(substr(unitid,1,1)) when 'G' then 1 when 'F' then 1 when 'P' then 2 when 'B' then 3 when 'H' then 4 when 'R' then 5 else 6 end;
+# run ruby stacking.rb
 
 # Export Views
 drop view cartoMoves;
 #create view cartoDB as select p.unitid, u.name, substr(u.id,1,1)||u.type as type, u.rank, u.chief, p.arrival, l.name as locality, l.latitude, l.longitude from positions p left join units u on u.id=p.unitid left join locations l on l.id=p.localityid order by p.line;
 create view cartoMoves as select case(part) when 0 then m.unitid else m.unitid || '-' || part end as unitid, icon.id as icon, army, m.step, m.datetime, m.flat as latitude, m.flong as longitude from moves m left join units u on u.id=m.unitid left join icons icon on icon.code=substr(m.unitid,1,1)||u.type||'B' order by m.unitid, m.step;
-.once csv/CoBMoves20b.csv
+.once csv/CoBMoves28.csv
 select * from cartoMoves;
 
 
 .exit
-
-
-
